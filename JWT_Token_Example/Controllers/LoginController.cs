71a845a82e6529970a4ddc64497467774e298fec
Lucky Pathan <lckpathan@gmail.com>
2024-01-12 15:08:27 +0530

    Add project files.

---
+++ JWT_Token_Example/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using JWT_Token_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWT_Token_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        private Users AuthenticateUser(Users users)
+        {
+            Users _users = null;
+            if(users.Username  == "admin" && users.Password == "12345") 
+            {
+                _users = new Users { Username = "Lucky" };
+            }
+            return _users;
+        }
+
+        private string GenerateToken(Users users)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],_config["Jwt:Audience"],null,
+                expires:DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users users) // accept users as perimiter and pass user to authancate fi=unction 
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(users);
+            if(user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
